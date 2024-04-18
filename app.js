686aa3a5e07230c24138b36911e9a202ab1bd486
hshehriyaralam <hshehriyaralam@gmail.com>
2024-04-18 05:22:04 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+// console.log("Hello SMIT")
+
+// //Data Types 
+// // String, Number, Boolean, Null and Undefined
+
+// // Strings
+// var username = "Shehriyar ALam";
+// console.log(username)
+
+
+//  var username = "'Jaffar aman'"
+//  console.log(username)
+
+// Number 
+// var num1 = 50;
+// var num2 = 55;
+// var sum  = num1 + num2;
+// console.log(sum)
+
+// var num1 = 50;
+// var num2 = 55;
+// var sum  = num1 - num2;
+// console.log(sum)
+
+// var num1 = 50;
+// var num2 = 55;
+// var sum  = num1 * num2;
+// console.log(sum)
+
+// var num1 = 50;
+// var num2 = 55;
+// var sum  = num1 / num2;
+// console.log(sum)
+
+
+// var num1 = 50;
+// var num2 = 55;
+// var sum  = num1 % num2;
+// console.log(sum)
+
+
+// // Cooncatenation
+// var FirstName = "Shehriyar"
+// var LastName = " Alam"
+// var addition = FirstName + LastName;
+// console.log(addition)
+
+/* Strings main Add nh Concate hota hai  */
+
+
+
+// var FirstName = "Shehriyar";
+// var LastName = "Alam"
+// var FullName = FirstName + "  " + LastName;
+// console.log(FullName);
+
+
+
+
+// DMAS
+// BODMAS
+// var num1 = 8;
+// var num2 = 9;
+// var ans = num1 + num2 * num2;
+// console.log(ans)
+
+// var num1 = 8;
+// var num2 = 9;
+// var ans = (num1 + num2) * num2;
+// console.log(ans)
+
+// var user = " ";
+// var UserName = ""; CamelCase >>>>Best Practice
+/* var $user = ;
+var user_name = ""; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
