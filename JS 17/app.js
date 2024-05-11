6077348654391feba97ac987690d87a8c2f484d9
hshehriyaralam <hshehriyaralam@gmail.com>
2024-05-11 14:13:35 -0600

    till Chapter 60

--- JS 17/app.js
+++ JS 17/app.js
@@ -8,3 +8,40 @@
 
 
 // apne program main koi data save krana hoga ap variable use kroge
+
+// var >> update & Re-declare 
+// let >> update , can't Re-declare 
+// const >> can't re-declare and update
+// line By line => interpretaor
+// translator => Compiler, Interpretor , 
+
+
+// 5 Data Types => primitive Data Types
+// string
+// number 
+// boolean
+// null
+// undefined 
+
+// non-primitive Data Types |Reference Type
+// array
+// object
+// function
+
+// Increment + Decrement ;
+// var num = 10;
+// console.log(num++);
+// console.log(num);
+
+// if Statement => Condition
+// var gendar = "male";
+
+
+// Arrays => Collection OF Data
+// var arr = []   => Empty array , Data Type =>>Array
+
+
+
+// for loop => Repitiion , most of the time array pr chlta hai
+// for loop mai  sirf Ap IF laga sakte ho else nh laga Sakte 
+
